53fcf16369f4a9a754d852055829a6c158cb01c2
sakeev92 <vs.sakeev@mail.ru>
2023-07-10 17:43:03 +0300

    HW3

---
+++ src/test/java/ru/hogwarts/school/Service/StudentServiceTest.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school.Service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("")
+public class StudentServiceTest {
+
+    @DisplayName("Правильная сортировка студентов по их возрасту")
+    @Test
+    public void test1() {
+    }
+}
